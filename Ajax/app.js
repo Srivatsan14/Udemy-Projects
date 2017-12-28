59e94dff32c75e77a182680a5549c60d0f313c94
Vivek <mvivek3112@gmail.com>
2017-12-28 21:53:40 +0530

    Ajax commit

---
+++ Ajax/app.js
@@ -0,0 +1,13 @@
+document.getElementById('button').addEventListener("click",getdata);
+function getdata(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data.txt', true);
+    xhr.onload = function() {
+        console.log("ReadyState",xhr.responseState);
+        if( this.status === 200){
+            console.log(this.responseText);
+            document.getElementById('output').innerHTML = `<h1> ${this.responseType}</h1>`;
+        }
+    };
+    xhr.send();
+}
