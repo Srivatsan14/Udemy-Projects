65a5308d7c88c343bbdec58a3e4449b93a0b8e54
Vivek <mvivek3112@gmail.com>
2017-12-28 22:07:03 +0530

    Ajax Final Commit

--- Ajax/app.js
+++ Ajax/app.js
@@ -3,10 +3,10 @@ function getdata(){
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'data.txt', true);
     xhr.onload = function() {
-        console.log("ReadyState",xhr.responseState);
+        console.log("ReadyState",xhr.readyState);
         if( this.status === 200){
             console.log(this.responseText);
-            document.getElementById('output').innerHTML = `<h1> ${this.responseType}</h1>`;
+            document.getElementById('output').innerHTML = `<h1> ${this.responseText}</h1>`;
         }
     };
     xhr.send();
