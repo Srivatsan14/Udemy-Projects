e5a7ca832e7ff098128c25d1fd528cc42b310c05
Vivek <mvivek3112@gmail.com>
2018-01-05 19:53:58 +0530

    second  commit

--- githubfinder/github.js
+++ githubfinder/github.js
@@ -1,5 +1,20 @@
 class Github{
     constructor(){
-        this.client_id=
+        this.client_id="c5d9979a77a11886b6b2";
+        this.client_secret='258a36db2e8f201f55c8e0b94edfc5c931bfa84a';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return{
+            profile,
+            repos
+        }
+
     }
 }
