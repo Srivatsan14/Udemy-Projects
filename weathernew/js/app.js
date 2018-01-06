26830e4626e30ab3c34d931e03ed59d7ecccc938
Vivek <mvivek3112@gmail.com>
2018-01-07 00:16:49 +0530

    api setup

--- weathernew/js/app.js
+++ weathernew/js/app.js
@@ -2,3 +2,14 @@ $(document).ready(function(){
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('.modal-trigger').leanModal();
 });
+const weather = new Weather('Chennai','TN');
+
+document.addEventListener('DOMContentLoaded',getWeather);
+
+function getWeather(){
+  weather.getWeather()
+    .then(results => {
+      console.log(results);
+    })
+    .catch(err => console.log(err));
+}
