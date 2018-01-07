356ba52a3caacdd77e485fa8722e24e714221003
Vivek <mvivek3112@gmail.com>
2018-01-07 12:41:52 +0530

    pre final commit

--- weathernew/js/app.js
+++ weathernew/js/app.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
-  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+ 
   $('.modal-trigger').leanModal();
 });
 const weather = new Weather('Chennai','TN');
 const ui = new UI();
 document.addEventListener('DOMContentLoaded',getWeather);
+document.getElementById('w-change-btn').addEventListener('click',(e)=>{
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  weather.changeLocation(city,state);
+
+  getWeather();
+  
+  });
+ 
+
 
 function getWeather(){
   weather.getWeather()
